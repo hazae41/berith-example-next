33847be8caefa2bf6c0a01c931a028e2cd8f77a1
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2022-11-03 10:56:56 +0100

    berith

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
-import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import '../styles/globals.css'
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props
+
   return <Component {...pageProps} />
 }
